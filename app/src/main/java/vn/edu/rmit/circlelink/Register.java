f3006852990c7df4019445c82284d6feb5bfa017
Vu Minh Ha <s3978681@rmit.edu.vn>
2025-01-10 13:47:15 +0700

    Add input validation & Get input data from user successfully (can check at Logcat)

--- app/src/main/java/vn/edu/rmit/circlelink/Register.java
+++ app/src/main/java/vn/edu/rmit/circlelink/Register.java
@@ -2,32 +2,23 @@ package vn.edu.rmit.circlelink;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ProgressBar;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -40,10 +31,9 @@ public class Register extends AppCompatActivity implements AdapterView.OnItemSel
     DatePickerDialog datePickerDialog;
     TextView textViewSignIn;
     TextInputEditText editTextUsername, editTextEmail, editTextPassword, editTextFname, editTextLname, editTextPhone;
+    String sex;
     FirebaseAuth mAuth;
     ProgressBar progressBar;
-    RadioGroup radioGroup;
-    RadioButton radioButton;
 
     @Override
     public void onStart() {
@@ -74,15 +64,7 @@ public class Register extends AppCompatActivity implements AdapterView.OnItemSel
         btnDatePicker = findViewById(R.id.btnDatePicker);
         textViewSignIn = findViewById(R.id.textViewSignIn);
         progressBar = findViewById(R.id.progressBar);
-        radioGroup = findViewById(R.id.radioGroup);
 
-
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                radioButton = findViewById(checkedId);
-            }
-        });
 
         spinner.setOnItemSelectedListener(this);
 
@@ -104,9 +86,8 @@ public class Register extends AppCompatActivity implements AdapterView.OnItemSel
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                progressBar.setVisibility(View.VISIBLE);
-
                 String username, email, password, fname, lname, phone;
+
                 username = editTextUsername.getText().toString().trim();
                 email = editTextEmail.getText().toString().trim();
                 password = editTextPassword.getText().toString().trim();
@@ -115,64 +96,65 @@ public class Register extends AppCompatActivity implements AdapterView.OnItemSel
                 phone = editTextPhone.getText().toString().trim();
 
                 if (TextUtils.isEmpty(username)) {
-                    editTextUsername.setError("Please enter a username");
-//                    Toast.makeText(Register.this, "Please enter a username", Toast.LENGTH_SHORT).show();
-//                    return;
+                    Toast.makeText(Register.this, "Please enter a username", Toast.LENGTH_SHORT).show();
+                    return;
                 }
 
                 if (TextUtils.isEmpty(email) || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                    editTextEmail.setError("Please enter a valid email address");
-//                    Toast.makeText(Register.this, "Please enter an email address", Toast.LENGTH_SHORT).show();
-//                    return;
-
+                    Toast.makeText(Register.this, "Please enter an email address", Toast.LENGTH_SHORT).show();
+                    return;
                 }
 
                 if (TextUtils.isEmpty(password)) {
-                    editTextPassword.setError("Please enter a password");
-//                    Toast.makeText(Register.this, "Please enter a password", Toast.LENGTH_SHORT).show();
-//                    return;
-
+                    Toast.makeText(Register.this, "Please enter a password", Toast.LENGTH_SHORT).show();
+                    return;
                 }
 
                 if (TextUtils.isEmpty(fname)) {
-                    editTextFname.setError("Please enter your first name");
-//                    Toast.makeText(Register.this, "Please enter your first name", Toast.LENGTH_SHORT).show();
-//                    return;
-
+                    Toast.makeText(Register.this, "Please enter your first name", Toast.LENGTH_SHORT).show();
+                    return;
                 }
 
                 if (TextUtils.isEmpty(lname)) {
-                    editTextLname.setError("Please enter your last name");
-//                    Toast.makeText(Register.this, "Please enter your last name", Toast.LENGTH_SHORT).show();
-//                    return;
-
+                    Toast.makeText(Register.this, "Please enter your last name", Toast.LENGTH_SHORT).show();
+                    return;
                 }
 
                 if (TextUtils.isEmpty(phone)) {
-                    editTextPhone.setError("Please enter your last name");
-//                    Toast.makeText(Register.this, "Please enter your last name", Toast.LENGTH_SHORT).show();
-//                    return;
+                    Toast.makeText(Register.this, "Please enter your last name", Toast.LENGTH_SHORT).show();
+                    return;
                 }
 
-                mAuth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                progressBar.setVisibility(View.GONE);
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(Register.this, "Account created.",
-                                            Toast.LENGTH_SHORT).show();
-                                    Intent intent = new Intent(Register.this, Login.class);
-                                    startActivity(intent);
-                                    finish();
-                                } else {
+                progressBar.setVisibility(View.VISIBLE);
 
-                                    Toast.makeText(Register.this, "Sign up failed.",
-                                            Toast.LENGTH_SHORT).show();
+                Log.d("Username: ", username);
+                Log.d("Email: ", email);
+                Log.d("Password: ", password);
+                Log.d("First name: ", fname);
+                Log.d("Last name: ", lname);
+                Log.d("Phone: ", phone);
+                Log.d("Sex: ", sex);
+                Log.d("Birthdate: ", btnDatePicker.getText().toString().trim());
 
-                                }
-                            }
-                        });
+//                mAuth.createUserWithEmailAndPassword(email, password)
+//                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<AuthResult> task) {
+//                                progressBar.setVisibility(View.GONE);
+//                                if (task.isSuccessful()) {
+//                                    Toast.makeText(Register.this, "Account created.",
+//                                            Toast.LENGTH_SHORT).show();
+//                                    Intent intent = new Intent(Register.this, Login.class);
+//                                    startActivity(intent);
+//                                    finish();
+//                                } else {
+//
+//                                    Toast.makeText(Register.this, "Sign up failed.",
+//                                            Toast.LENGTH_SHORT).show();
+//
+//                                }
+//                            }
+//                        });
             }
         });
     }
@@ -242,20 +224,13 @@ public class Register extends AppCompatActivity implements AdapterView.OnItemSel
     }
 
     public void openDatePicker(View view) {
-        datePickerDialog.setOnShowListener(new DialogInterface.OnShowListener() {
-            @Override
-            public void onShow(DialogInterface dialog) {
-                WindowManager.LayoutParams lp = datePickerDialog.getWindow().getAttributes();
-                lp.dimAmount = 0.7f;
-                datePickerDialog.getWindow().setAttributes(lp);
-                datePickerDialog.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
-            }
-        });
         datePickerDialog.show();
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        sex = parent.getItemAtPosition(position).toString();
     }
 
     @Override
