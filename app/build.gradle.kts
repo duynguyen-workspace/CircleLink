952cf18abc43adb0edf72a15ab9c745904e02af3
hly-22 <s3979290@rmit.edu.vn>
2025-01-11 10:41:04 +0700

    update Glide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("com.github.bumptech.glide:glide:4.12.0")        // for processing images
-    annotationProcessor("com.github.bumptech.glide:glide:4.12.0")       // for processing images
+    implementation(libs.glide)        // for processing images
+    annotationProcessor(libs.glide)       // for processing images
 
 }
